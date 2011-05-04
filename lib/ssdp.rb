8e9e2ea587eb28ad48bcb7ea8e0c27e287386b0d
Steve Loveless <steve.loveless@gmail.com>
2011-05-03 22:00:14 -0700

    Moved Socket patches to core_ext file

--- lib/ssdp.rb
+++ lib/ssdp.rb
@@ -30,7 +30,6 @@ class SSDP
   class Error < StandardError
   end
 
-
   # Default broadcast address
   BROADCAST = '239.255.255.250'
 
@@ -425,21 +424,4 @@ ST: #{search_target}\r
   end
 end
 
-# :stopdoc:
 
-# Workaround for mising constants on Windows
-
-module Socket::Constants
-  IP_ADD_MEMBERSHIP = 12 unless defined? IP_ADD_MEMBERSHIP
-  IP_MULTICAST_LOOP = 11 unless defined? IP_MULTICAST_LOOP
-  IP_MULTICAST_TTL  = 10 unless defined? IP_MULTICAST_TTL
-  IP_TTL            =  4 unless defined? IP_TTL
-end
-
-class Socket
-  IP_ADD_MEMBERSHIP = 12 unless defined? IP_ADD_MEMBERSHIP
-  IP_MULTICAST_LOOP = 11 unless defined? IP_MULTICAST_LOOP
-  IP_MULTICAST_TTL  = 10 unless defined? IP_MULTICAST_TTL
-  IP_TTL            =  4 unless defined? IP_TTL
-end
-# :startdoc:
